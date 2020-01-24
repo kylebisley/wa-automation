dca875f8f12329941ad63aca0de9f140baf7c52a
Kyle Bisley <kylebisley@gmail.com>
2020-01-24 14:55:43 -0800

    Working on event api calls
    Currently pulls those registered and checks to see if they are checked
    in.
    Needs to have more vigor in choosing the event.

---
+++ application.py
@@ -0,0 +1,43 @@
+# find appropiate new applicants night
+    #pull list of New Applicant Nights
+    #if event was held this month
+    #pull attendance list
+# get list of those in attendance
+# for each attendie 
+    # set to requested level
+    # set pending new
+    # generate invoice
+    # email invoice
+from pprint import pprint
+from datetime import datetime
+from config import config
+import pdb
+
+def auto_applications(session, contacts, now):
+    temp = f'events?'
+    events = session.request('GET', temp)
+    #pprint(events)
+    for event in events['Events']:
+        if event['Name'] == 'Applicants Evening':
+            print('************************************************************')           
+            #pprint(event)
+            print(event['Name'])
+            temp = f'events/{event["Id"]}'
+            event_details = session.request('GET', temp)
+            print('************************************************************')
+            #pprint(event_details)
+            
+            temp =f'eventregistrations/?eventId={event["Id"]}'
+            event_reg = session.request('GET', temp)
+            print('************************************************************')
+            print('************************************************************')
+            pprint(event_reg)
+            
+            for registration in event_reg:
+                if registration['IsCheckedIn']:
+                    print(registration['Contact']['Name'])
+                    print('Is Checked IN!!!!!!!')
+    
+
+    print('onewards and upwards')
+#events[event][0]['Name']
